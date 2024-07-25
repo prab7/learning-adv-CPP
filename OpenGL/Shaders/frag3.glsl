2af73226184a7059e08046f2042063d87077e4c7
prab <nicksingh1109@gmail.com>
2024-07-25 17:02:14 +0530

    learned about uniform vars

--- OpenGL/Shaders/frag3.glsl
+++ OpenGL/Shaders/frag3.glsl
@@ -4,7 +4,10 @@ in vec3 v_vertexColors;
 
 out vec4 color;
 
+uniform float u_Offset; // uniform variable
+
+// Entry point of program
 void main()
 {
-    color = vec4(v_vertexColors.r, v_vertexColors.g, v_vertexColors.b, 1.f);
+    color = vec4(v_vertexColors.r + u_Offset, v_vertexColors.g, v_vertexColors.b, 1.f);
 }
--- OpenGL/Shaders/vert3.glsl
+++ OpenGL/Shaders/vert3.glsl
@@ -1,7 +1,9 @@
-#version 410 core
+#version 420 core
 
 layout(location=0) in vec3 position; 
 layout(location=1) in vec3 vertexColors; 
+
+uniform float u_Offset;  // uniform variables
 
 out vec3 v_vertexColors;
 
@@ -9,5 +11,5 @@ void main()
 {
     v_vertexColors = vertexColors;
 
-    gl_Position = vec4(position.x, position.y, position.z, 1.0f);
+    gl_Position = vec4(position.x, position.y + u_Offset, position.z, 1.0f);
 }
