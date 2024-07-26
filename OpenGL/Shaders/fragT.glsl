3ec1ac7d2aa4197dd55c6f08dd75b19a411022f8
prab <nicksingh1109@gmail.com>
2024-07-26 12:58:40 +0530

    learned about perspective

--- OpenGL/Shaders/fragT.glsl
+++ OpenGL/Shaders/fragT.glsl
@@ -5,10 +5,11 @@ in vec3 v_vertexColors;
 out vec4 color;
 
 uniform mat4 u_ModelMatrix; // uniform model matrix
+uniform mat4 u_Perspective; // uniform perpective matrix
 
 // Entry point of program
 void main()
 {
-    vec4 newVertexColors = u_ModelMatrix * vec4(v_vertexColors, 1.0f);
-    color = vec4(newVertexColors.r, newVertexColors.g, newVertexColors.b, 1.f);
+    vec4 newVertexColors = vec4(v_vertexColors, 1.0f);
+    color = newVertexColors;
 }
--- OpenGL/Shaders/vertT.glsl
+++ OpenGL/Shaders/vertT.glsl
@@ -4,14 +4,15 @@ layout(location=0) in vec3 position;
 layout(location=1) in vec3 vertexColors; 
 
 uniform mat4 u_ModelMatrix; // uniform model matrix
+uniform mat4 u_Perspective;  // uniform perspective matrix 
 
 out vec3 v_vertexColors;
 
 void main()
 {
     v_vertexColors = vertexColors;
+    
+    vec4 newPosition = u_Perspective * u_ModelMatrix * vec4(position, 1.0f);
 
-    vec4 newPosition = u_ModelMatrix * vec4(position, 1.0f);
-
-    gl_Position = vec4(newPosition.x, newPosition.y, newPosition.z, 1.0f);
+    gl_Position = newPosition;
 }
